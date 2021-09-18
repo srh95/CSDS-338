fa86dcaa97baff0ccbbdc9cd99d74fa38a8c89d6
srh95 <72227777+srh95@users.noreply.github.com>
2021-09-17 22:11:15 -0400

    Create orphan.c

---
+++ orphan.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+pid_t p;
+p=fork();
+
+if(p==0)
+     {
+         sleep(5); //child goes to sleep and in the mean time parent terminates
+	 printf("\n ");
+         printf("I am child having PID %d\n",getpid());
+         printf("My parent PID is %d\n",getppid());
+     }
+     else
+     {
+         printf("I am parent having PID %d\n",getpid());
+         printf("My child PID is %d\n",p);
+     }
+ } 
